c575c2638a45fc5748658c80e41a18e08df9d11a
gilbc890 <gilbc890@gmail.com>
2017-11-21 20:40:09 +0900

    11/21/17 jQuery effects-scroll page, navi fadeIn/fadeOut

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
 $(document).ready(function(){
   $('.glyphicon-menu-hamburger').click(function(){
-    $('.navi').removeClass('hide');
-  });
+    $('.navi').fadeIn(1000)
+  })
   $('.glyphicon-remove').click(function(){
-    $('.navi').addClass('hide');
+    $('.navi').fadeOut(1000)
   });
+
+  // Add smooth scrolling to all links
+    $(".navi_button").on('click', function(event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 1200, function(){
+
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+
+      $('.navi').fadeOut()
+    });
+
+
+
+
 
 });
